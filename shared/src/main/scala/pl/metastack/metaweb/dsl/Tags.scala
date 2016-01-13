e1f53126ea2a8b4b34e678854e82ce49865d0a95
Tim Nieradzik <tim@metastack.pl>
2016-01-13 20:18:34 +0100

    DSL: Add hide()

--- shared/src/main/scala/pl/metastack/metaweb/dsl/Tags.scala
+++ shared/src/main/scala/pl/metastack/metaweb/dsl/Tags.scala
@@ -4,6 +4,14 @@ import pl.metastack.metaweb.tag.HTMLTag
 
 object Tags {
   implicit class HTMLTagExtensions(tag: HTMLTag) {
+    /** Sets `style` to `display: none` if `state` is true, otherwise
+      * sets `style` to `showDisplay` (empty string if None).
+      */
+    def hide(state: Boolean, showDisplay: Option[Display] = None): Unit =
+      tag.style := (
+        if (state) Display.None.property
+        else showDisplay.map(_.property).getOrElse(""))
+
     def show(state: Boolean, cssTags: String*): Unit = {
       tag.`class`.update { cur =>
         val currentTags = cur.split(' ')
