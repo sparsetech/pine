c360125d466511b5ce7311741d3a612569c5d930
Tim Nieradzik <tim@metastack.pl>
2016-05-01 01:58:00 +0200

    HtmlParser: Cosmetic changes

--- jvm/src/main/scala/pl/metastack/metaweb/HtmlParser.scala
+++ jvm/src/main/scala/pl/metastack/metaweb/HtmlParser.scala
@@ -5,8 +5,7 @@ import pl.metastack.metaweb.macros.Helpers
 import scala.xml.XML
 
 object HtmlParser {
-  def convert(node: scala.xml.Node,
-              root: Boolean): tree.Node = {
+  def convert(node: scala.xml.Node, root: Boolean): tree.Node =
     (node.label, Option(node.prefix)) match {
       case ("#PCDATA", _) => new pl.metastack.metaweb.tree.Text(node.text)
       case (tag, prefix) =>
@@ -17,9 +16,8 @@ object HtmlParser {
 
         val tagAttrs = node.attributes.asAttrMap ++ rootAttributes
         val tagChildren = node.child.map(convert(_, root = false))
-        new pl.metastack.metaweb.tree.Tag(tagName, tagAttrs, Map.empty, tagChildren)
+        tree.Tag(tagName, tagAttrs, Map.empty, tagChildren)
     }
-  }
 
   def fromString(html: String): tree.Tag = {
     val xml = XML.loadString(html)
