e1dce71d84f859853825f0805fa0af9a31ea5fa6
Tim Nieradzik <tim@metastack.pl>
2016-08-03 17:20:11 +0200

    Node: Simplify updateByTag()

--- shared/src/main/scala/pl/metastack/metaweb/tree/Node.scala
+++ shared/src/main/scala/pl/metastack/metaweb/tree/Node.scala
@@ -135,10 +135,8 @@ trait Tag extends Node {
     instantiateMap(nodes.toMap)
 
   def updateByTag[U <: Tag](f: U => Tag)(implicit ct: ClassTag[U]): Node =
-    if (getClass == ct.runtimeClass) f(asInstanceOf[U])
-    else map {
-      case t: Tag => t.updateByTag[U](f)
-      case n      => n
+    partialMap {
+      case n if n.getClass == ct.runtimeClass => f(n.asInstanceOf[U])
     }
 
   def updateById[U <: Tag](id: String, f: U => Node): Node = {
