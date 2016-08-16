9d6b35aad99e0d4ecae0b5bcb41d8400bcdebbed
Tim Nieradzik <tim@metastack.pl>
2016-08-16 16:13:39 +0200

    Text: Improve rendering

--- shared/src/main/scala/pl/metastack/metaweb/tree/render/Text.scala
+++ shared/src/main/scala/pl/metastack/metaweb/tree/render/Text.scala
@@ -6,9 +6,15 @@ import pl.metastack.metaweb.tree
 trait Text[N <: tree.Node] extends tree.Render[N, String]
 
 object Text extends Text[tree.Node] {
+  // TODO Don't mutate global state
+  private var lineBreak = false
+
   trait Implicit {
     implicit class NodeToText(node: tree.Node) {
-      def toText: String = Text.render(node)
+      def toText: String = {
+        lineBreak = false
+        Text.render(node).trim
+      }
     }
   }
 
@@ -20,16 +26,38 @@ object Text extends Text[tree.Node] {
 
   implicit case object RenderTag extends Text[tree.Tag] {
     def render(node: tree.Tag): String = {
-      val isLiteral = node.isInstanceOf[tag.Script]
-      val children =
-        if (isLiteral) Seq.empty
-        else node.children.map(Text.render)
+      def children(): String =
+        node.children.map { child =>
+          if (!lineBreak) Text.render(child)
+          else {
+            val text = Text.render(child).span(_.isSpaceChar)._2
+            lineBreak = false
+            text
+          }
+        }.mkString
 
-      children.mkString
+      node match {
+        case t: tag.Script => ""
+        case t: tag.Br =>
+          lineBreak = true
+          "\n"
+        case t: tag.Ul =>
+          node.children.map {
+            case li: tag.Li => "- " + Text.render(li).trim + "\n"
+            case n => ""
+          }.mkString + "\n" + { lineBreak = true; "" }
+        case _: tag.Div =>
+          children() + "\n" + { lineBreak = true; "" }
+        case _: tag.H1 | _: tag.H2 | _: tag.H3 | _: tag.H4 | _: tag.H5 | _: tag.H6 =>
+          children() + "\n\n" + { lineBreak = true; "" }
+        case t: tag.P =>
+          children() + "\n\n" + { lineBreak = true; "" }
+        case t: tree.Tag => children()
+      }
     }
   }
 
   implicit case object RenderText extends Text[tree.Text] {
-    def render(node: tree.Text): String = node.text
+    def render(node: tree.Text): String = node.text.replaceAll("\\s+", " ")
   }
 }
---
+++ shared/src/test/scala/pl/metastack/metaweb/TextSpec.scala
@@ -0,0 +1,75 @@
+package pl.metastack.metaweb
+
+import org.scalatest.FunSuite
+
+class TextSpec extends FunSuite {
+  test("Text node") {
+    val node = tree.Text("Hello\nworld")
+    assert(node.toText == "Hello world")
+  }
+
+  test("Text node (2)") {
+    val node = tree.Text("Hello\tworld")
+    assert(node.toText == "Hello world")
+  }
+
+  test("Text node (3)") {
+    val node = tree.Text("Hello \t\n world")
+    assert(node.toText == "Hello world")
+  }
+
+  test("Script node") {
+    val node = html"""<script>Hello world</script>"""
+    assert(node.toText == "")
+  }
+
+  test("List node") {
+    val node = html"""<ul><li>Hello</li><li>World</li></ul>"""
+    assert(node.toText == "- Hello\n- World")
+  }
+
+  test("List node (2)") {
+    val node = html"""<ul> <li>Hello</li> <li>World</li> </ul>"""
+    assert(node.toText == "- Hello\n- World")
+  }
+
+  test("List node (3)") {
+    val node = html"""<ul> <li> Hello </li> <li> World </li> </ul>"""
+    assert(node.toText == "- Hello\n- World")
+  }
+
+  test("Div node") {
+    val node = html"""<div>Hello</div>"""
+    assert(node.toText == "Hello")
+  }
+
+  test("Div nodes") {
+    val node = html"""<div><div>Hello</div><div>World</div></div>"""
+    assert(node.toText == "Hello\nWorld")
+  }
+
+  test("Heading nodes") {
+    val node = html"""<div><h1>Heading</h1>Text</div>"""
+    assert(node.toText == "Heading\n\nText")
+  }
+
+  test("Heading nodes (2)") {
+    val node = html"""<div><h1>Heading</h1><p>Text</p></div>"""
+    assert(node.toText == "Heading\n\nText")
+  }
+
+  test("Heading nodes (3)") {
+    val node = html"""<div><h1>Heading</h1><p>Text</p><h1>Heading</h1><p>Text</p></div>"""
+    assert(node.toText == "Heading\n\nText\n\nHeading\n\nText")
+  }
+
+  test("Paragraph nodes") {
+    val node = html"""<div><p>First</p><p>Second</p></div>"""
+    assert(node.toText == "First\n\nSecond")
+  }
+
+  test("Line break node") {
+    val node = html"""<div>Hello<br /> world</div>"""
+    assert(node.toText == "Hello\nworld")
+  }
+}
