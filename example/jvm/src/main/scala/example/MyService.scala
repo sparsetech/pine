6f30f5dd72628dfba4b3ec459435c44a54aaae01
Tim Nieradzik <tim@sparse.tech>
2017-05-08 20:42:00 +0200

    Remove example project

--- example/jvm/src/main/scala/example/MyService.scala
+++
@@ -1,8 +0,0 @@
-package example
-
-import scala.concurrent.Future
-
-object MyService extends Service[Request, Response] {
-  override val process: PartialFunction[Request, Future[Response]] =
-    (new service.NumberGuess).compose(new service.Books).process
-}
--- example/project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.12")
-
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.8.0")
